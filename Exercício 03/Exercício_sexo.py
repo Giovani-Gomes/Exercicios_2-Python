b1e3a03b7061758e29fd8d151df94e93912a51a5
Giovani-Gomes <75649962+Giovani-Gomes@users.noreply.github.com>
2021-12-24 18:48:36 -0300

    Exercício 03
    Resolução do Exercício 03(Python)  da 2° Lista de Exercício.

---
+++ Exercício 03/Exercício_sexo.py
@@ -0,0 +1,10 @@
+Sexo = str(input("Informe seu sexo M - Masculino e F - Para Feminino "))
+
+if(Sexo == 'H' or Sexo == 'h'):
+    print("Você me informou que é um Homem")
+elif(Sexo == 'F' or Sexo == 'f'):
+    print("Você me informou que é uma Mulher")
+else:
+    print("Sexo Inválido")
+
+    
