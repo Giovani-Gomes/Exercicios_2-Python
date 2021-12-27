7476014fdf89667e7ecb64a80f4673c776607c82
Giovani Gomes <giovanigomes2002@gmail.com>
2021-12-26 21:22:13 -0300

    Exercício 04
    Resolução do Exercício 04(Python) da segunda Lista de Exercício.

---
+++ Exercício 04/Consoante_Vogal.py
@@ -0,0 +1,6 @@
+Letra = str(input("Digite uma Letra: ").upper())
+
+if Letra == 'A' or Letra == 'E' or Letra == 'I' or Letra == 'O' or Letra == 'U':
+    print("Você Digitou uma Vogal")
+else:
+    print("Você Digitou uma Consoante")
